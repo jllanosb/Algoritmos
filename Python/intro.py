194b8d275641018123f4dcd16816474321787ef3
Jaime Llanos <jllanosb@unc.edu.pe>
2026-04-23 17:10:12 -0500

    Subiendo archivo python

---
+++ Python/intro.py
@@ -0,0 +1,25 @@
+# Fundamentos Basicos Python
+
+# Comentario en una sola linea usar # 
+# Comentario Multilinea """ """
+"""
+Este es un comentario multilinea
+"""
+
+# Declarar Variables
+""" 
+nombre_identifica valor
+edad_persona
+edad = int
+mensaje = 
+"""
+
+# Tipo de dato
+edad = 18
+print(edad)
+
+mensaje = "Bienvenido a UPN"
+print(mensaje)
+
+# Presentacion mas avanzada uso f' '
+print(f'Usted tiene {edad} años. {mensaje}')
