1cea9d3da4b93faf3b85993d034820596361bd41
Jaime Llanos <jllanosb@unc.edu.pe>
2026-05-07 17:38:43 -0500

    Agregando Ejercicio Calcular velocidad en python

---
+++ Python/2_Calcular_Velocidad.py
@@ -0,0 +1,6 @@
+#
+distancia = int (input ("Ingrese la distancia recorrida: "))
+tiempo=int (input("Ingrese el tiempo de viaje: "))
+velocidad = distancia/tiempo
+
+print(f' La Velocidad del Viaje es {velocidad} m/s')
