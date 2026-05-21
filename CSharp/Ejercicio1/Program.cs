1b42b89e2c98102256332f2c9d84bdbb67ba1ea4
Jaime Llanos <jllanosb@unc.edu.pe>
2026-05-21 16:39:20 -0500

    Definiendo variables, entrada de datos y decision

--- CSharp/Ejercicio1/Program.cs
+++ CSharp/Ejercicio1/Program.cs
@@ -10,6 +10,33 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
+            //Fecha y Hora
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("LLanos Bardales, Jaime");//Programador
+            Console.WriteLine("Ejercicio 1 - Edad Persona");
+
+            //Declarar Variables
+            int edad;
+
+            //Entrada de Datos
+            //Mostrar mensaje al usuario para ingresar datos
+            Console.WriteLine("Ingrese su edad: ");
+            //Primera forma de guardar el valor ingresado por el usuario
+            // Usando int.Parse
+            edad = int.Parse(Console.ReadLine());
+
+            //Decision
+            /* Si -->if edad >= 18 Entonces {
+             *      Mostrar "Mayor de Edad"
+             * sino
+             *      Mostrar "Menor de Edad"
+             * finsi }
+             */
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres Mayor de Edad");
+            }
+
         }
     }
 }
