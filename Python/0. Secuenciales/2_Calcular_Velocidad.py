ca6cc1575b0fe1b02342ebaf3e2cbe9416f2285c
Jaime Llanos <jllanosb@unc.edu.pe>
2026-06-11 17:30:28 -0500

    Actualizando ejercicios

--- Python/0. Secuenciales/2_Calcular_Velocidad.py
+++
@@ -1,6 +0,0 @@
-#
-distancia = int (input ("Ingrese la distancia recorrida: "))
-tiempo=int (input("Ingrese el tiempo de viaje: "))
-velocidad = distancia/tiempo
-
-print(f' La Velocidad del Viaje es {velocidad} m/s')
