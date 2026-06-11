fbd568494326f6afa1ec04cca0bc74ddf268ee3e
Jaime Llanos <jllanosb@unc.edu.pe>
2026-06-11 12:29:18 -0500

    Ejercicio 3 C. Anidado

---
+++ 3. Condicionales Anidados/Ejercicio2CAnidado.py
@@ -0,0 +1,18 @@
+# Definir variables
+
+precio=0.50
+pago = 0
+# Entrada datos
+cantidad=int(input("Ingrese la cantidad de manzanas: "))
+
+# Decision
+if (cantidad > 100):
+    dscto = 0.80
+    pago = cantidad * precio * dscto
+elif (cantidad > 20):
+    dscto = 0.90
+    pago = cantidad*precio*dscto
+else:
+    pago = cantidad * precio;
+
+print(f"Por {cantidad} manzanas, pagaré s/. {pago}")
