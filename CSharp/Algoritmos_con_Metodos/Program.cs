7056a17e9750109bd8de0e039e964db8c686dfea
Jaime Llanos <jllanosb@unc.edu.pe>
2026-06-25 17:00:13 -0500

    Caso 1

--- CSharp/Algoritmos_con_Metodos/Program.cs
+++ CSharp/Algoritmos_con_Metodos/Program.cs
@@ -30,7 +30,55 @@ namespace Algoritmos_con_Metodos
                 Console.Write("Seleccione una opcion: ");
                 opcion = int.Parse(Console.ReadLine());
                 Console.Clear(); //Limpiar la pantalla
+                //Condicional Multiple
+                switch (opcion) {
+                    case 0: Console.WriteLine("Saliendo del sistema..."); break;
+                    case 1: DescuentoTienda(); break;
+                    case 2: ClasificarEstudiantes(); break;
+                    case 3: TipoViaje(); break;
+                    case 4: ClasificarProductos(); break;
+                    case 5: CategoriaVehiculos(); break;
+                    case 6: DecisionTransporte(); break;
+                    default: Console.WriteLine("Opcion Incorrecta"); break;
+                }
+                Console.ReadKey();//Pausar para mostrar resultados
+                Console.Clear(); //Limpiar la pantalla
             } while (opcion != 0);
+        }
+        //Metodo para el caso 1
+        static void DescuentoTienda() {
+            double descuento;
+            Console.WriteLine("Descuento en Tienda");
+            Console.WriteLine("Ingrese el monto de compra");
+            var monto = int.Parse(Console.ReadLine());
+            //condicional anidado
+            if (monto > 1000) { 
+                descuento = monto*0.20;
+            }
+            else if (monto > 500) {
+                descuento = monto * 0.10;
+            }
+            else {
+                descuento = 0;
+            }
+            Console.WriteLine($"El descuento obtenido es s/. {descuento} ");
+            Console.WriteLine($"Monto a pagar es s/. {monto - descuento}");
+        }
+        static void ClasificarEstudiantes() { 
+        
+        }
+        static void TipoViaje() { 
+            
+        }
+        static void ClasificarProductos()
+        {
+
+        }
+        static void CategoriaVehiculos() { 
+
+        }
+        static void DecisionTransporte() { 
+            
         }
     }
 }
