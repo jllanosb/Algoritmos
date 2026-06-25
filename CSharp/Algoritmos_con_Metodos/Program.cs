2ed11c441cb9d8f7095e20026ec3d3befd68003e
Jaime Llanos <jllanosb@unc.edu.pe>
2026-06-25 16:42:28 -0500

    Algoritmos con Metodos

---
+++ CSharp/Algoritmos_con_Metodos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos_con_Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metodo Menu
+            MenuOpciones();
+        }
+        //Metodo Menu
+        static void MenuOpciones() {
+            int opcion;
+            //Estructura repetitiva Do While 
+            do {
+                Console.WriteLine(" ### Menu Casos Algoritmos ### ");
+                Console.WriteLine(DateTime.Now);
+                Console.WriteLine("1. Descuento Tienda");
+                Console.WriteLine("2. Clasificacion Estudiantes");
+                Console.WriteLine("3. Tipo de Viaje");
+                Console.WriteLine("4. Clasificacion Productos");
+                Console.WriteLine("5. Categoria Vehiculos");
+                Console.WriteLine("6. Decision Transporte");
+                Console.WriteLine("0. Salir");
+                Console.Write("Seleccione una opcion: ");
+                opcion = int.Parse(Console.ReadLine());
+                Console.Clear(); //Limpiar la pantalla
+            } while (opcion != 0);
+        }
+    }
+}
